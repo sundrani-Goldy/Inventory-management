a4e004385edadeb42921ff11dba368511ee9b111
sundrani-Goldy <goldysundrani143@gmail.com>
2024-03-08 12:05:25 +0530

    Revert "change schemas"
    This reverts commit 0dc9f5c2307ba4e948b99d84048678a2544c3284.

--- store_app/schema.py
+++ store_app/schema.py
@@ -1,18 +1,43 @@
 import graphene
-from .objects import *
+from graphene_django.types import DjangoObjectType
+from .models import Product, ProductImage
+
+class ProductType(DjangoObjectType):
+    class Meta:
+        model = Product
+
+class ProductImageType(DjangoObjectType):
+    class Meta:
+        model = ProductImage
 
 class Query(graphene.ObjectType):
-    all_customers = graphene.List(CustomerType)
     all_products = graphene.List(ProductType)
-
-    def resolve_all_customers(self, info, **kwargs):
-        return Customer.objects.all()
 
     def resolve_all_products(self, info, **kwargs):
         return Product.objects.all()
 
+class ProductInput(graphene.InputObjectType):
+    name = graphene.String()
+    description = graphene.String()
+    base_price = graphene.String()
+    sell_price = graphene.String()
+    mrp = graphene.String()
+    # Add other fields as needed
+
+
+class CreateProduct(graphene.Mutation):
+    class Arguments:
+        input = ProductInput(required=True)
+
+    product = graphene.Field(ProductType)
+
+    def mutate(self, info, input):
+        product_data = input
+        product = Product.objects.create(**product_data)
+        return CreateProduct(product=product)
+
+
 class Mutation(graphene.ObjectType):
-    create_customer = CreateCustomer.Field()
     create_product = CreateProduct.Field()
 
 schema = graphene.Schema(query=Query, mutation=Mutation)
--- store_app/urls.py
+++ store_app/urls.py
@@ -8,6 +8,7 @@ from django.urls import path, include, re_path
 from drf_yasg.views import get_schema_view
 from rest_framework import routers, permissions
 from drf_yasg import openapi
+from store_app.views.customer import CustomerView
 from store_app.schema import *
 
 from graphene_django.views import GraphQLView
