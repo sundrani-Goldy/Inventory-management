75fcf3dc3ff2e9e628de07bfcab093635d348c10
Goldy Sundrani <goldy.devstree@gmail.com>
2024-03-20 15:11:44 +0530

    updated the code

--- store_app/views/inventory.py
+++ store_app/views/inventory.py
@@ -6,6 +6,24 @@ from store_app.serializers.inventory import InventorySerializer
 from rest_framework.permissions import IsAdminUser
 from rest_framework.authentication import TokenAuthentication
 from drf_yasg.utils import swagger_auto_schema
+
+from drf_yasg import openapi
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework import status
+import logging
+import os
+from rest_framework.permissions import IsAdminUser,IsAuthenticated
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.response import Response
+from django.http import HttpResponseServerError
+from rest_framework import status
+from store_app.serializers.inventory import InventorySerializer,InventoryLogSerializer
+from store_app.models.inventory_and_warehouse.inventory import Inventory,InventoryLog
+from rest_framework.exceptions import ValidationError
+from store_app.models.product import Product
+from store_app.models.inventory_and_warehouse.warehouse import Warehouse
+from store_app.models.product_detail import Tag
 class InventoryView(ModelViewSet):
     serializer_class = InventorySerializer
     queryset = Inventory.objects.all()
