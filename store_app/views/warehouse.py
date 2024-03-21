481359251399651af34a9a6368abee539a2e608a
Goldy Sundrani <goldy.devstree@gmail.com>
2024-03-21 12:09:33 +0530

    Bug fixing & warehouse delete api update

--- store_app/views/warehouse.py
+++ store_app/views/warehouse.py
@@ -15,7 +15,12 @@ from django.http import HttpResponseServerError
 from rest_framework import status
 from store_app.serializers.warehouse import WarehouseSerializer,WarehouseInventorySerializer
 from store_app.models.inventory_and_warehouse.warehouse import Warehouse,WarehouseInventory,OtherDetail
-
+from store_app.models.product import Product
+from store_app.models.inventory_and_warehouse.warehouse import Warehouse
+from store_app.models.product_detail import Tag
+from store_app.serializers.inventory import InventorySerializer,InventoryLogSerializer
+from store_app.models.inventory_and_warehouse.inventory import Inventory,InventoryLog
+from rest_framework.exceptions import ValidationError
 
 class WarehouseView(ModelViewSet):
     serializer_class = WarehouseSerializer
@@ -24,6 +29,24 @@ class WarehouseView(ModelViewSet):
     authentication_classes = [TokenAuthentication]
 
 
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        
+        # Check if there's only one warehouse left
+        if Warehouse.objects.count() == 1:
+            return Response({"Cannot delete the only remaining warehouse."}, status=status.HTTP_400_BAD_REQUEST)
+
+        self.perform_destroy(instance)
+        return Response( "Warehouse Deleted Successfully",status=status.HTTP_204_NO_CONTENT)
+
+    @swagger_auto_schema(auto_schema=None)
+    def partial_update(self, request, *args, **kwargs):
+        pass
+
+
+        
+
+    
 class WarehouseInventoryView(ModelViewSet):
     serializer_class = WarehouseInventorySerializer
     queryset = WarehouseInventory.objects.all()
