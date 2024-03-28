f5717477bc11d4d56f877fc1f75a3dda0ec73d4b
Goldy Sundrani <goldy.devstree@gmail.com>
2024-03-28 12:27:03 +0530

    bug fixing

--- store_app/views/warehouse.py
+++ store_app/views/warehouse.py
@@ -8,7 +8,7 @@ from rest_framework.response import Response
 from rest_framework import status
 import logging
 import os
-from rest_framework.permissions import IsAdminUser,IsAuthenticated
+from rest_framework.permissions import IsAdminUser
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.response import Response
 from django.http import HttpResponseServerError
@@ -17,9 +17,6 @@ from store_app.serializers.warehouse import WarehouseSerializer,WarehouseInvento
 from store_app.models.inventory_and_warehouse.warehouse import Warehouse,WarehouseInventory,OtherDetail
 from store_app.models.product import Product
 from store_app.models.inventory_and_warehouse.warehouse import Warehouse
-from store_app.models.product_detail import Tag
-from store_app.serializers.inventory import InventorySerializer,InventoryLogSerializer
-from store_app.models.inventory_and_warehouse.inventory import Inventory,InventoryLog
 from django.db.models.signals import post_save
 from store_app.views.inventory import create_inventory_log,create_or_update_inventory
 
