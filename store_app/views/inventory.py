8dc4c0c754518eb44cb0fdbe911bc80066fdee1e
Goldy Sundrani <goldy.devstree@gmail.com>
2024-03-28 13:27:02 +0530

    bug fixing

--- store_app/views/inventory.py
+++ store_app/views/inventory.py
@@ -56,3 +56,29 @@ def create_inventory_log(instance,old_quantity,reason):
     # Instead of updating directly, add tags one by one
     for tag in tags:
         inventory_log.fk_tag.add(tag)
+
+def create_or_update_inventory(instance):
+    product = instance.fk_product
+    inventories = WarehouseInventory.objects.filter(fk_product=product)
+
+    print("DEBUG: Number of inventories found:", inventories.count())
+
+    # Aggregate quantities across all warehouses
+    total_available_quantity = inventories.aggregate(total_available=Sum('available_quantity'))['total_available'] or 0
+    total_allotted_quantity = inventories.aggregate(total_allotted=Sum('allotted_quantity'))['total_allotted'] or 0
+    total_total_quantity = inventories.aggregate(total_total=Sum('total_quantity'))['total_total'] or 0
+    total_sold_quantity = inventories.aggregate(total_sold=Sum('sold_quantity'))['total_sold'] or 0
+    total_on_hand = inventories.aggregate(total_on_hand=Sum('on_hand'))['total_on_hand'] or 0
+
+    print("DEBUG: Aggregated quantities - available:", total_available_quantity, "allotted:", total_allotted_quantity, "total:", total_total_quantity, "sold:", total_sold_quantity, "on_hand:", total_on_hand)
+
+    # Create or update the inventory record
+    inventory, created = Inventory.objects.get_or_create(fk_product=product)
+    inventory.available_quantity = total_available_quantity
+    inventory.allotted_quantity = total_allotted_quantity
+    inventory.total_quantity = total_total_quantity
+    inventory.sold_quantity = total_sold_quantity
+    inventory.on_hand = total_on_hand
+    inventory.save()
+
+    print("DEBUG: Inventory created or updated -", inventory)
